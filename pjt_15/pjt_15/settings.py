51c0e18e5b016fb11b051e51f74032b35f749946
riumr <atrium_grate.0j@icloud.com>
2022-11-04 15:03:03 +0900

    'django-extensions' 오타수정

--- pjt_15/pjt_15/settings.py
+++ pjt_15/pjt_15/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "djagngo-extensions",
+    "django-extensions",
     "accounts",
     "django.contrib.admin",
     "django.contrib.auth",
